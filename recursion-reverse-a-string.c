1e83c0baaf14e502285137b713d1dd0347dda278
ahan-strak <ahanabubakar@gmail.com>
2021-09-30 20:25:04 +0530

    first commit

---
+++ recursion-reverse-a-string.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void reverse_s(char c[], int n)
+{
+    if (c[n] == '\0')
+        return;
+    reverse_s(c, n + 1);
+    printf("%c", c[n]);
+}
+int main()
+{
+    char s[10], res;
+    fgets(s, 10, stdin);
+    reverse_s(s, 0);
+    // printf("%s", res);
+}
